2e1b1665d2cfd5c32f0cf3de35636d505268d7ca
Animesh <swdev.animesh@gmail.com>
2023-09-11 00:19:33 +0530

    files added

---
+++ mern/Blog/client/src/components/account/Logins.js
@@ -0,0 +1,36 @@
+import React from "react";
+// import { Box, TextField, Button, styled } from "@mui/material";
+
+// const LoginComponent = styled(Box)`
+//   width: 400px;
+//   box-shadow: 50px 46px 27px -36px rgba(133, 133, 133, 0.45);
+//   border: 1px solid #858585;
+//   margin: 10vh auto 0 auto;
+// `;
+
+// const Image = styled("img")({
+//   width: "30px",
+//   height: "90px",
+// });
+
+const Login = (props) => {
+  // const img =
+  //   "https://images.freeimages.com/variants/2B3oqERNAaMavsuRKQtTn4jH/f4a36f6589a0e50e702740b15352bc00e4bfaf6f58bd4db850e167794d05993d";
+
+  return (
+    
+    // <Box>
+    //   {/* <Image src={img} alt="Login logo"></Image> */}
+
+    //   <LoginComponent>
+    //     <TextField m={10} variant="outlined" label="User Name"></TextField>
+    //     <TextField variant="outlined" label="Password"></TextField>
+    //     <Button variant="contained">Sign In</Button>
+    //     <p className="text-center text-secondary">OR</p>
+    //     <Button variant="contained">Create new Account</Button>
+    //     <Button variant="contained">Create new Account</Button>
+    //   </LoginComponent>
+    // </Box>
+  );
+};
+export default Login;
